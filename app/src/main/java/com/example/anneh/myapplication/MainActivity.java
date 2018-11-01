7077c9784f671792dd552cd1aa4ff56cf552ce1c
annehs <annehoogerduijn@gmail.com>
2018-11-01 16:13:04 +0100

    woensdag 1-11-2018

--- app/src/main/java/com/example/anneh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/anneh/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void checkClicked(View v) {
         Log.d("potato", "checkClicked");
+
+        CheckBox checkbox = (CheckBox) v;
+        String checkbox_string = checkbox.getText().toString();
+
+        image_dict = {} ??
+
+        ImageView image = (ImageView) findViewById(R.id.arms);
+
+        if (checkbox.isChecked()) {
+            image = visible
+                    set.Visibility(View.VISIBLE);
+        }
+        else {
+            image = invisible
+                    set.Visibility(View.INVISIBLE);
+        }
     }
 }
